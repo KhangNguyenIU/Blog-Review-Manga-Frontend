b35b6f62876b9f088ed35deec2e5d4d9c74393f9
KhangNguyen <nguyenduykhangcd12t4@gmail.com>
2021-09-08 20:44:24 +0700

    tool upload photo

--- pages/test.js
+++ pages/test.js
@@ -1,8 +1,12 @@
 import { IconButton } from '@material-ui/core'
 import { nanoid } from 'nanoid'
 import React, { useEffect, useRef, useState } from 'react'
+// import { AiOutLinePlus } from 'react-icons/ai'
 import ContentEditable from 'react-contenteditable'
 import ToolEditor from '../Components/BlogEditor/ToolEditor'
+import { AiOutlineCloudUpload, AiOutlinePlus } from 'react-icons/ai'
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+
 
 const Test = () => {
     const [positionX, setPositionX] = useState(0)
@@ -12,14 +16,14 @@ const Test = () => {
     const [selected, setSelected] = useState({})
     const [previewedImage, setPreviewedImage] = useState('')
     const [isUploadImage, setIsUploadImage] = useState(false)
-    const [elements, setElements] = useState([{ id: "first-line", html: "" }])
+    const [elements, setElements] = useState([{ id: "first-line", html: "", open: true }])
     const refs = useRef([])
 
-useEffect(()=>{
-    if(focused){
-        refs.current[focused].focus()
-    }
-},[focused])
+    useEffect(() => {
+        if (focused) {
+            refs.current[focused].focus()
+        }
+    }, [focused])
     function saveSelection() {
         if (window.getSelection) {
             var sel = window.getSelection();
@@ -33,29 +37,33 @@ useEffect(()=>{
     }
 
     const handleChange = (index) => e => {
-        console.log("change", e.target.value);
+        // console.log("change", e.target.value);
         elements[index].html = e.target.value
+        if (e.target.value.length > 1) {
+            elements[index].open = false
+        } else {
+            elements[index].open = true
+        }
         setElements([...elements])
     }
 
     const handleKeyDown = (index) => e => {
         if (e.keyCode == 13) {
             e.preventDefault()
-            elements.splice(index + 1, 0, { id: nanoid(3), html: '' })
+            elements.splice(index + 1, 0, { id: nanoid(3), html: '', open: true })
             setElements([...elements])
-            setFocused(index+1)
+            setFocused(index + 1)
         }
-        if (e.keyCode == 8 && elements[index].html == "") {
+        if (e.keyCode == 8 && elements[index].html == "" && index != 0) {
             elements.splice(index, 1)
             setElements([...elements])
-            setFocused(index-1)
-
+            setFocused(index - 1)
         }
     }
 
 
     const handleMouseDown = (index) => e => {
-        // console.log("eMouseDown", e);
+        console.log("eMouseDown", e);
 
 
         if (openTool == false) {
@@ -63,7 +71,7 @@ useEffect(()=>{
             let tempPositionY = e.layerY == undefined ? e.nativeEvent.layerY : e.layerY
             console.log("mousedonw", tempPositionX, tempPositionY);
             setPositionX(tempPositionX)
-            setPositionY(tempPositionY - 65)
+            setPositionY(tempPositionY - 50)
         }
     }
 
@@ -81,7 +89,7 @@ useEffect(()=>{
         // console.log(selection);
 
         if (!document.getSelection().isCollapsed && openTool == false) {
-            setPositionX(((relativePositionX + positionX) / 2) -60    )
+            setPositionX(((relativePositionX + positionX) / 2) - 60)
             setOpenTool(true)
         } else {
             setOpenTool(false)
@@ -114,18 +122,20 @@ useEffect(()=>{
     const ToolUploadImage = (props) => {
         return (
             <React.Fragment>
-                <div className={` ${props.open ? "" : "hidden"}`}
-                    style={{ position: 'absolute', left: '-60px', top: "-5px" }}
+                <div
+                    style={{ position:'absolute', left:'-50px' , margin:'auto'}}
                 >
-                    <input 
-                     accept="image/*"  
-                     id="upload-body-image" 
-                     hidden type="file"
-                     onChange={handlePreviewImage(props.index)}
-                     />
+                    {/* <h1>Upload imagge</h1> */}
+                    <input
+                        accept="image/*"
+                        id="upload-body-image"
+                        hidden
+                        type="file"
+                        onChange={handlePreviewImage(props.index)}
+                    />
                     <label htmlFor="upload-body-image">
-                        <IconButton size="medium" component="span">
-                            <AiOutlinePlus className="icon-small" />
+                        <IconButton>
+                            <AiOutlinePlus />
                         </IconButton>
 
                     </label>
@@ -156,28 +166,39 @@ useEffect(()=>{
 
 
             <div className="container relative">
-                {
-                    elements.map((el, index) => (
-                        <div key={index}
-                            onMouseDown={handleMouseDown(index)}
-                            onMouseUp={handleMouseUp(index)}>
-                            <ContentEditable
-                                key={index}
-                                className="text-editor"
-                                id={el.id}
-                                contentEditable={true}
-                                placeholder={el.id}
-                                html={el.html}
-                                onChange={handleChange(index)}
-                                onKeyDown={handleKeyDown(index)}
-                                innerRef ={(curRef)=> refs.current[index] = curRef}
-                            >
-                                {/* {el.value} */}
-                            </ContentEditable>
-                        </div>
+                <div>
+                    {
+                        elements.map((el, index) => (
+                            <div key={index}
+                                style={{ display: 'flex'}}
+                                onMouseDown={handleMouseDown(index)}
+                                onMouseUp={handleMouseUp(index)}>
 
-                    ))
-                }
+                                {
+                                    el.open && <ToolUploadImage
+                                        style={{marginleft:'-5rem'}}
+                                        open={el.open}
+                                        index={index} />
+                                }
+                                <ContentEditable
+                                    key={index}
+                                    className="text-editor"
+                                    id={el.id}
+                                    style={{width: '100%', marginleft:"4rem"}}
+                                    contentEditable={true}
+                                    placeholder={el.id}
+                                    html={el.html}
+                                    onChange={handleChange(index)}
+                                    onKeyDown={handleKeyDown(index)}
+                                    innerRef={(curRef) => refs.current[index] = curRef}
+                                >
+
+                                </ContentEditable>
+
+                            </div>
+                        ))
+                    }
+                </div>
 
                 <ToolEditor
                     openTool={openTool}
